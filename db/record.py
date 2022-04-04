20f55c2168ef8f5b63cd8c5f616b0aa8eba12188
kagarise <3255385205@qq.com>
2022-04-04 15:33:28 +0800

    添加统计

--- db/record.py
+++ db/record.py
@@ -10,7 +10,6 @@ class Record(Base):
     black = Column(String(20), nullable=False, comment="黑棋玩家")
     white = Column(String(20), nullable=False, comment="白棋玩家")
     step_data = Column(Text, nullable=False, comment="落子数据")
-    board_data = Column(Text, nullable=False, comment="棋盘数据")
     like = Column(Integer, default=0, nullable=True, comment="获赞数")
     start_time = Column(DateTime, nullable=False, comment="开始时间")
     end_time = Column(DateTime, nullable=False, comment="结束时间")
@@ -18,13 +17,14 @@ class Record(Base):
     create_time = Column(DateTime, server_default=func.now(), nullable=False, comment="创建时间")
     update_time = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False, comment="修改时间")
 
-    def __init__(self, winner=None, black=None, white=None, step_data=None, board_data=None):
+    def __init__(self, winner=None, black=None, white=None, step_data=None, start_time=None, end_time=None):
         self.winner = winner
         self.black = black
         self.white = white
         self.step_data = step_data
-        self.board_data = board_data
         self.like = 0
+        self.start_time = start_time
+        self.end_time = end_time
 
     def __repr__(self):
         return f'<Record {self.id!r}>'
@@ -36,7 +36,6 @@ class Record(Base):
             'black': self.black,
             'white': self.white,
             'step_data': self.step_data,
-            'board_data': self.board_data,
             'like': self.like,
             'start_time': self.start_time,
             'end_time': self.end_time,
